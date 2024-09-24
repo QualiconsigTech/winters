9959d3738a0ff4312d13549d5c1038341273873a
unknown <ezequiel.beserra@qualiconsig.com.br>
2024-09-24 10:16:34 -0300

    Adicionado novo link ao Rank Historico

--- src/mock/data.ts
+++ src/mock/data.ts
@@ -3,13 +3,12 @@
 export const QualiconsigOutput = [
   {title: "Ranking Diario", url: "https://app.powerbi.com/reportEmbed?reportId=0fdd5198-c5b5-4cbe-b235-43a66fed1a58&autoAuth=true&ctid=83bc0f1e-f029-4f4f-a62a-401fb961118e"},
   {title: "Ranking Diario + CIP", url: "https://app.powerbi.com/reportEmbed?reportId=fa718ef9-c963-426b-b632-7e68b767c36f&autoAuth=true&ctid=83bc0f1e-f029-4f4f-a62a-401fb961118e"},
-  {title: "Rank Historico", url: "https://app.powerbi.com/reportEmbed?reportId=b1655a22-c1b5-40d1-9c7d-3eef33303a08&autoAuth=true&ctid=83bc0f1e-f029-4f4f-a62a-401fb961118e"},
+  {title: "Rank Historico", url: "https://app.powerbi.com/reportEmbed?reportId=0a50e7e5-e7fe-4d7e-a63c-105ff7d895bf&autoAuth=true&ctid=83bc0f1e-f029-4f4f-a62a-401fb961118e"},
   {title: "Assinaturas", url: "https://app.powerbi.com/reportEmbed?reportId=a4078798-7440-46e8-bd9f-3cbc58dd1669&autoAuth=true&ctid=83bc0f1e-f029-4f4f-a62a-401fb961118e"},
   {title: "Retorno CIP", url: "https://app.powerbi.com/reportEmbed?reportId=b55d49ce-f31a-4fe5-bf82-7b8cceeda7a6&autoAuth=true&ctid=83bc0f1e-f029-4f4f-a62a-401fb961118e"},
   {title: "Esteira CIP", url: "https://app.powerbi.com/reportEmbed?reportId=1e53fc7a-1608-4d8a-9754-b6dd9761de62&autoAuth=true&ctid=83bc0f1e-f029-4f4f-a62a-401fb961118e"},
   {title: "Esteira", url: "https://app.powerbi.com/reportEmbed?reportId=7f3c4d43-99dd-42a9-8812-97cfa2942097&autoAuth=true&ctid=83bc0f1e-f029-4f4f-a62a-401fb961118e"},
   {title: "Produção Diaria", url: "https://app.powerbi.com/reportEmbed?reportId=cf566c7a-b4d4-4d8f-a60f-c48be8a4a807&autoAuth=true&ctid=83bc0f1e-f029-4f4f-a62a-401fb961118e"},
-  {title: "Rank Historico", url: "https://app.powerbi.com/reportEmbed?reportId=0a50e7e5-e7fe-4d7e-a63c-105ff7d895bf&autoAuth=true&ctid=83bc0f1e-f029-4f4f-a62a-401fb961118e"},
 ]
 
 export const Parceiros = [
