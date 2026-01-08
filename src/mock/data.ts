66aedff4680f24a36a6ce6319ab6d5f9547fe092
unknown <pedro.aquino@qualiconsig.com.br>
2026-01-08 10:39:53 -0300

    Removido alguns botoes

--- src/mock/data.ts
+++ src/mock/data.ts
@@ -1,38 +1,94 @@
-
-
 export const QualiconsigOutput = [
-  {title: "Ranking Diario", url: "https://app.powerbi.com/reportEmbed?reportId=71e2ba62-f598-4605-ac3e-7ee5f4ff9fd1&autoAuth=true&ctid=83bc0f1e-f029-4f4f-a62a-401fb961118e"},
-  {title: "Rank Historico - 2025", url: "https://app.powerbi.com/reportEmbed?reportId=30e400ac-b050-48fd-a8a3-fea468e8c370&autoAuth=true&ctid=83bc0f1e-f029-4f4f-a62a-401fb961118e"},
-  {title: "Rank Historico - 2024", url: "https://app.powerbi.com/reportEmbed?reportId=0a50e7e5-e7fe-4d7e-a63c-105ff7d895bf&autoAuth=true&ctid=83bc0f1e-f029-4f4f-a62a-401fb961118e"},
-  {title: "Assinaturas", url: "https://app.powerbi.com/reportEmbed?reportId=a4078798-7440-46e8-bd9f-3cbc58dd1669&autoAuth=true&ctid=83bc0f1e-f029-4f4f-a62a-401fb961118e"},
-  {title: "Retorno CIP", url: "https://app.powerbi.com/reportEmbed?reportId=b55d49ce-f31a-4fe5-bf82-7b8cceeda7a6&autoAuth=true&ctid=83bc0f1e-f029-4f4f-a62a-401fb961118e"},
-  {title: "Esteira CIP", url: "https://app.powerbi.com/reportEmbed?reportId=4d569da4-b3b5-4725-8d3d-45e32474caea&autoAuth=true&ctid=83bc0f1e-f029-4f4f-a62a-401fb961118e"},
-  {title: "Digitação Diaria", url: "https://app.powerbi.com/reportEmbed?reportId=cf566c7a-b4d4-4d8f-a60f-c48be8a4a807&autoAuth=true&ctid=83bc0f1e-f029-4f4f-a62a-401fb961118e"},
-  {title: "PRODUÇÃO MARGEM E CARTÃO", url: "https://app.powerbi.com/reportEmbed?reportId=78f82242-3f9d-4825-975f-d150ba7af1f3&autoAuth=true&ctid=83bc0f1e-f029-4f4f-a62a-401fb961118e"},
-  {title: "Ranking Qualispeed 5.0", url: "https://app.powerbi.com/reportEmbed?reportId=135ce861-f878-4694-b5b6-50e2785bc7f2&autoAuth=true&ctid=83bc0f1e-f029-4f4f-a62a-401fb961118e"},
-  {title: "Campanha Assinaturas", url: "https://app.powerbi.com/reportEmbed?reportId=a088fe25-21e7-4f1e-9cf4-5792afc13a37&autoAuth=true&ctid=83bc0f1e-f029-4f4f-a62a-401fb961118e"}
-]
+  {
+    title: "Ranking Diario",
+    url: "https://app.powerbi.com/reportEmbed?reportId=71e2ba62-f598-4605-ac3e-7ee5f4ff9fd1&autoAuth=true&ctid=83bc0f1e-f029-4f4f-a62a-401fb961118e",
+  },
+  {
+    title: "Rank Historico - 2025",
+    url: "https://app.powerbi.com/reportEmbed?reportId=30e400ac-b050-48fd-a8a3-fea468e8c370&autoAuth=true&ctid=83bc0f1e-f029-4f4f-a62a-401fb961118e",
+  },
+  {
+    title: "Rank Historico - 2024",
+    url: "https://app.powerbi.com/reportEmbed?reportId=0a50e7e5-e7fe-4d7e-a63c-105ff7d895bf&autoAuth=true&ctid=83bc0f1e-f029-4f4f-a62a-401fb961118e",
+  },
+  {
+    title: "Assinaturas",
+    url: "https://app.powerbi.com/reportEmbed?reportId=a4078798-7440-46e8-bd9f-3cbc58dd1669&autoAuth=true&ctid=83bc0f1e-f029-4f4f-a62a-401fb961118e",
+  },
+  {
+    title: "Retorno CIP",
+    url: "https://app.powerbi.com/reportEmbed?reportId=b55d49ce-f31a-4fe5-bf82-7b8cceeda7a6&autoAuth=true&ctid=83bc0f1e-f029-4f4f-a62a-401fb961118e",
+  },
+  {
+    title: "Esteira CIP",
+    url: "https://app.powerbi.com/reportEmbed?reportId=4d569da4-b3b5-4725-8d3d-45e32474caea&autoAuth=true&ctid=83bc0f1e-f029-4f4f-a62a-401fb961118e",
+  },
+  {
+    title: "Digitação Diaria",
+    url: "https://app.powerbi.com/reportEmbed?reportId=cf566c7a-b4d4-4d8f-a60f-c48be8a4a807&autoAuth=true&ctid=83bc0f1e-f029-4f4f-a62a-401fb961118e",
+  },
+  {
+    title: "PRODUÇÃO MARGEM E CARTÃO",
+    url: "https://app.powerbi.com/reportEmbed?reportId=78f82242-3f9d-4825-975f-d150ba7af1f3&autoAuth=true&ctid=83bc0f1e-f029-4f4f-a62a-401fb961118e",
+  },
+];
 
 export const Parceiros = [
-  {title: "Top 10 | Diario", url: "https://app.powerbi.com/reportEmbed?reportId=f88daec5-421d-4aa2-afb9-0cfbd6c8f011&autoAuth=true&ctid=83bc0f1e-f029-4f4f-a62a-401fb961118e"},
-  {title: "Q20 - Parceiros", url: "https://app.powerbi.com/reportEmbed?reportId=98dea945-8913-42da-9db3-07788bddf777&autoAuth=true&ctid=83bc0f1e-f029-4f4f-a62a-401fb961118e"},
-  {title: "Retorno CIP", url: "https://app.powerbi.com/reportEmbed?reportId=0c4fb576-b745-4402-9e38-a6d70403c335&autoAuth=true&ctid=83bc0f1e-f029-4f4f-a62a-401fb961118e"},
-  {title: "Digitação Diaria", url: "https://app.powerbi.com/reportEmbed?reportId=ccff6aa1-5ac2-42d4-946a-7c9c9fb6e59e&autoAuth=true&ctid=83bc0f1e-f029-4f4f-a62a-401fb961118e"},
-  {title: "Rank Diario", url: "https://app.powerbi.com/reportEmbed?reportId=f4cd1d73-03dd-4d12-bc59-23e839851e59&autoAuth=true&ctid=83bc0f1e-f029-4f4f-a62a-401fb961118e"}
-]
+  {
+    title: "Top 10 | Diario",
+    url: "https://app.powerbi.com/reportEmbed?reportId=f88daec5-421d-4aa2-afb9-0cfbd6c8f011&autoAuth=true&ctid=83bc0f1e-f029-4f4f-a62a-401fb961118e",
+  },
+  {
+    title: "Q20 - Parceiros",
+    url: "https://app.powerbi.com/reportEmbed?reportId=98dea945-8913-42da-9db3-07788bddf777&autoAuth=true&ctid=83bc0f1e-f029-4f4f-a62a-401fb961118e",
+  },
+  {
+    title: "Retorno CIP",
+    url: "https://app.powerbi.com/reportEmbed?reportId=0c4fb576-b745-4402-9e38-a6d70403c335&autoAuth=true&ctid=83bc0f1e-f029-4f4f-a62a-401fb961118e",
+  },
+  {
+    title: "Digitação Diaria",
+    url: "https://app.powerbi.com/reportEmbed?reportId=ccff6aa1-5ac2-42d4-946a-7c9c9fb6e59e&autoAuth=true&ctid=83bc0f1e-f029-4f4f-a62a-401fb961118e",
+  },
+  {
+    title: "Rank Diario",
+    url: "https://app.powerbi.com/reportEmbed?reportId=f4cd1d73-03dd-4d12-bc59-23e839851e59&autoAuth=true&ctid=83bc0f1e-f029-4f4f-a62a-401fb961118e",
+  },
+];
 
 export const Digital = [
-  {title: "Performance", url: "https://app.powerbi.com/reportEmbed?reportId=a5f5e577-eba8-40c4-bf77-9ba018e570b9&autoAuth=true&ctid=83bc0f1e-f029-4f4f-a62a-401fb961118e"},
-  {title: "Assinaturas", url: "https://app.powerbi.com/reportEmbed?reportId=a4078798-7440-46e8-bd9f-3cbc58dd1669&autoAuth=true&ctid=83bc0f1e-f029-4f4f-a62a-401fb961118e"},
-  {title: "Esteira CIP", url: "https://app.powerbi.com/reportEmbed?reportId=c8edc0fc-6e7e-4f64-84eb-5d78da71fdc0&autoAuth=true&ctid=83bc0f1e-f029-4f4f-a62a-401fb961118e"},
-]
+  {
+    title: "Performance",
+    url: "https://app.powerbi.com/reportEmbed?reportId=a5f5e577-eba8-40c4-bf77-9ba018e570b9&autoAuth=true&ctid=83bc0f1e-f029-4f4f-a62a-401fb961118e",
+  },
+  {
+    title: "Assinaturas",
+    url: "https://app.powerbi.com/reportEmbed?reportId=a4078798-7440-46e8-bd9f-3cbc58dd1669&autoAuth=true&ctid=83bc0f1e-f029-4f4f-a62a-401fb961118e",
+  },
+  {
+    title: "Esteira CIP",
+    url: "https://app.powerbi.com/reportEmbed?reportId=c8edc0fc-6e7e-4f64-84eb-5d78da71fdc0&autoAuth=true&ctid=83bc0f1e-f029-4f4f-a62a-401fb961118e",
+  },
+];
 
 export const Treinamento = [
-  {title: "Acompanhamento novos consultores", url: "https://app.powerbi.com/reportEmbed?reportId=507c42e3-9359-46ed-b261-ca9892103df6&autoAuth=true&ctid=83bc0f1e-f029-4f4f-a62a-401fb961118e"},
-]
+  {
+    title: "Acompanhamento novos consultores",
+    url: "https://app.powerbi.com/reportEmbed?reportId=507c42e3-9359-46ed-b261-ca9892103df6&autoAuth=true&ctid=83bc0f1e-f029-4f4f-a62a-401fb961118e",
+  },
+];
 
 export const Backoffice = [
-  {title: "VENDAS DIÁRIAS - PARCEIRO", url: "https://app.powerbi.com/reportEmbed?reportId=8517a9cc-6c27-45cc-8c9a-dd0bd5f3fd93&autoAuth=true&ctid=83bc0f1e-f029-4f4f-a62a-401fb961118e"},
-  {title: "ESTEIRA BACKOFFICE", url: "https://app.powerbi.com/reportEmbed?reportId=a07d536f-c676-4d0c-99d1-313ec0c511cc&autoAuth=true&ctid=83bc0f1e-f029-4f4f-a62a-401fb961118e"},
-  {title: "¨¨¨¨", url: "https://app.powerbi.com/reportEmbed?reportId=a07d536f-c676-4d0c-99d1-313ec0c511cc&autoAuth=true&ctid=83bc0f1e-f029-4f4f-a62a-401fb961118e"},
-]
+  {
+    title: "VENDAS DIÁRIAS - PARCEIRO",
+    url: "https://app.powerbi.com/reportEmbed?reportId=8517a9cc-6c27-45cc-8c9a-dd0bd5f3fd93&autoAuth=true&ctid=83bc0f1e-f029-4f4f-a62a-401fb961118e",
+  },
+  {
+    title: "ESTEIRA BACKOFFICE",
+    url: "https://app.powerbi.com/reportEmbed?reportId=a07d536f-c676-4d0c-99d1-313ec0c511cc&autoAuth=true&ctid=83bc0f1e-f029-4f4f-a62a-401fb961118e",
+  },
+  {
+    title: "¨¨¨¨",
+    url: "https://app.powerbi.com/reportEmbed?reportId=a07d536f-c676-4d0c-99d1-313ec0c511cc&autoAuth=true&ctid=83bc0f1e-f029-4f4f-a62a-401fb961118e",
+  },
+];
