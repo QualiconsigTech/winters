e5d4a160ee9a1b582beca65c7d5604370e7a2dea
unknown <pedro.aquino@qualiconsig.com.br>
2026-08-21 14:57:35 -0300

    remove old people analytics

--- src/mock/data.ts
+++ src/mock/data.ts
@@ -32,10 +32,6 @@ export const QualiconsigOutput = [
     url: "https://app.powerbi.com/reportEmbed?reportId=78f82242-3f9d-4825-975f-d150ba7af1f3&autoAuth=true&ctid=83bc0f1e-f029-4f4f-a62a-401fb961118e",
   },
   {
-    title: "People Analytics",
-    url: "https://app.powerbi.com/groups/me/reports/7451897e-311d-463d-b67a-1d20148695b4/6792068ee3e070e8078e?language=pt-BR&experience=power-bi",
-  },
-  {
     title: "People Analytics - BP",
     url: "https://app.powerbi.com/links/s0iDtQgd9M?ctid=83bc0f1e-f029-4f4f-a62a-401fb961118e&pbi_source=linkShare",
   },
