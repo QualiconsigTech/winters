2a305fd3e37b387847efbcb8809bccd9dd2215b2
unknown <pedro.aquino@qualiconsig.com.br>
2026-08-24 18:11:47 -0300

    remove people analitycs

--- src/mock/data.ts
+++ src/mock/data.ts
@@ -30,11 +30,7 @@ export const QualiconsigOutput = [
   {
     title: "PRODUÇÃO MARGEM E CARTÃO",
     url: "https://app.powerbi.com/reportEmbed?reportId=78f82242-3f9d-4825-975f-d150ba7af1f3&autoAuth=true&ctid=83bc0f1e-f029-4f4f-a62a-401fb961118e",
-  },
-  {
-    title: "People Analytics - BP",
-    url: "https://app.powerbi.com/links/s0iDtQgd9M?ctid=83bc0f1e-f029-4f4f-a62a-401fb961118e&pbi_source=linkShare",
-  },
+  }
 ];
 
 export const Parceiros = [
